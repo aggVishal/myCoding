3d7b57ecaa9bf7005a0afbb0f294e916037c675d
Vishalagg901 <aggarwalvishal901@gmail.com>
2020-07-04 23:40:28 +0530

    update

--- DP/c001.cpp
+++ DP/c001.cpp
@@ -941,6 +941,33 @@ int INV_bitonic(vector<int> &nums)
     }
     return Omax;
 }
+
+int LIS_leftToRight(vector<int> &nums)
+{
+    vector<int> dp(nums.size() + 1);
+    vector<int> no(nums.size() + 1);
+    int Omax = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        dp[i] = 1;
+        for (int j = i - 1; j >= 0; j--)
+        {
+            if (nums[j] < nums[i])
+            {
+                // dp[i] = max(dp[i], dp[j] + 1);
+                if(dp[i]==dp[j]+1){
+`                   no[j]++;
+                }
+                else if(dp[i]<dp[j]+1){
+                    no[j]=1;
+                    dp[i]=dp[j]+1;
+                }
+            }
+        }
+        Omax = max(Omax, dp[i]);
+    }
+    return Omax;
+}
 void solve()
 {
     // int n = 10;
