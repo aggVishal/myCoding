6564dd00726ed84f77aa2684ead47962f9e9d784
Vishalagg901 <aggarwalvishal901@gmail.com>
2020-07-07 17:07:34 +0530

    update

--- DP/c001.cpp
+++ DP/c001.cpp
@@ -3,195 +3,195 @@
 #include <algorithm>
 using namespace std;
 
-// //mazepath series...
+//mazepath series...
 
-// int MP_jumps(int sr, int sc, int er, int ec, vector<vector<int>> &dp)
-// {
-//     if (sr == er && sc == ec)
-//     {
-//         return dp[sr][sc] = 1;
-//     }
-//     if (dp[sr][sc] != 0)
-//         return dp[sr][sc];
+int MP_jumps(int sr, int sc, int er, int ec, vector<vector<int>> &dp)
+{
+    if (sr == er && sc == ec)
+    {
+        return dp[sr][sc] = 1;
+    }
+    if (dp[sr][sc] != 0)
+        return dp[sr][sc];
 
-//     int count = 0;
-//     int d = max(er - sr, ec - sc);
-//     for (int i = 1; i <= d; i++)
-//     {
-//         if (sr + i <= er)
-//         {
-//             count += MP_jumps(sr + i, sc, er, ec, dp);
-//         }
-//         if (sc + i <= ec)
-//         {
-//             count += MP_jumps(sr, sc + i, er, ec, dp);
-//         }
-//         if (sr + i <= er && sc + i <= ec)
-//         {
-//             count += MP_jumps(sr + i, sc + i, er, ec, dp);
-//         }
-//     }
-//     return dp[sr][sc] = count;
-// }
-// int MP_jumps_DP(int sr, int sc, int er, int ec, vector<vector<int>> &dp)
-// {
-//     for (int sr = er; sr >= 0; sr--)
-//     {
-//         for (sc = ec; sc >= 0; sc--)
-//         {
-//             if (sr == er && sc == ec)
-//             {
-//                 dp[sr][sc] = 1;
-//                 continue;
-//             }
+    int count = 0;
+    int d = max(er - sr, ec - sc);
+    for (int i = 1; i <= d; i++)
+    {
+        if (sr + i <= er)
+        {
+            count += MP_jumps(sr + i, sc, er, ec, dp);
+        }
+        if (sc + i <= ec)
+        {
+            count += MP_jumps(sr, sc + i, er, ec, dp);
+        }
+        if (sr + i <= er && sc + i <= ec)
+        {
+            count += MP_jumps(sr + i, sc + i, er, ec, dp);
+        }
+    }
+    return dp[sr][sc] = count;
+}
+int MP_jumps_DP(int sr, int sc, int er, int ec, vector<vector<int>> &dp)
+{
+    for (int sr = er; sr >= 0; sr--)
+    {
+        for (sc = ec; sc >= 0; sc--)
+        {
+            if (sr == er && sc == ec)
+            {
+                dp[sr][sc] = 1;
+                continue;
+            }
 
-//             int count = 0;
-//             int d = max(er - sr, ec - sc);
-//             for (int i = 1; i <= d; i++)
-//             {
-//                 if (sr + i <= er)
-//                 {
-//                     count += dp[sr + i][sc]; //MP_jumps(sr+i,sc,er,ec,dp);
-//                 }
-//                 if (sc + i <= ec)
-//                 {
-//                     count += dp[sr][sc + i]; //MP_jumps(sr,sc+i,er,ec,dp);
-//                 }
-//                 if (sr + i <= er && sc + i <= ec)
-//                 {
-//                     count += dp[sr + i][sc + i]; //MP_jumps(sr+i,sc,er,ec,dp);
-//                 }
-//             }
-//             dp[sr][sc] = count;
-//         }
-//     }
-//     return dp[0][0];
-// }
+            int count = 0;
+            int d = max(er - sr, ec - sc);
+            for (int i = 1; i <= d; i++)
+            {
+                if (sr + i <= er)
+                {
+                    count += dp[sr + i][sc]; //MP_jumps(sr+i,sc,er,ec,dp);
+                }
+                if (sc + i <= ec)
+                {
+                    count += dp[sr][sc + i]; //MP_jumps(sr,sc+i,er,ec,dp);
+                }
+                if (sr + i <= er && sc + i <= ec)
+                {
+                    count += dp[sr + i][sc + i]; //MP_jumps(sr+i,sc,er,ec,dp);
+                }
+            }
+            dp[sr][sc] = count;
+        }
+    }
+    return dp[0][0];
+}
 
-// int dice(int s, int e, vector<int> &dp)
-// {
-//     if (s == e)
-//     {
-//         return dp[s] = 1;
-//     }
-//     if (dp[s] != 0)
-//         return dp[s];
-//     int count = 0;
-//     for (int i = 1; i <= 6; i++)
-//     {
-//         if (s + i <= e)
-//         {
-//             count += dice(s + i, e, dp);
-//         }
-//     }
-//     return dp[s] = count;
-// }
-// int dice_DP(int s, int e, vector<int> &dp)
-// {
-//     for (s = e; s >= 0; s--)
-//     {
-//         if (s == e)
-//         {
-//             dp[s] = 1;
-//             continue;
-//         }
+int dice(int s, int e, vector<int> &dp)
+{
+    if (s == e)
+    {
+        return dp[s] = 1;
+    }
+    if (dp[s] != 0)
+        return dp[s];
+    int count = 0;
+    for (int i = 1; i <= 6; i++)
+    {
+        if (s + i <= e)
+        {
+            count += dice(s + i, e, dp);
+        }
+    }
+    return dp[s] = count;
+}
+int dice_DP(int s, int e, vector<int> &dp)
+{
+    for (s = e; s >= 0; s--)
+    {
+        if (s == e)
+        {
+            dp[s] = 1;
+            continue;
+        }
 
-//         int count = 0;
-//         for (int i = 1; i <= 6; i++)
-//         {
-//             if (s + i <= e)
-//             {
-//                 count += dp[s + i];
-//             }
-//         }
-//         dp[s] = count;
-//     }
-//     return dp[0];
-// }
+        int count = 0;
+        for (int i = 1; i <= 6; i++)
+        {
+            if (s + i <= e)
+            {
+                count += dp[s + i];
+            }
+        }
+        dp[s] = count;
+    }
+    return dp[0];
+}
 
-// int dice_random(int s, int e, vector<int> &ar, vector<int> &dp)
-// {
-//     if (s == e)
-//     {
-//         return dp[s] = 1;
-//     }
-//     if (dp[s] != 0)
-//         return dp[s];
-//     int count = 0;
-//     for (int i = 0; i < ar.size(); i++)
-//     {
-//         if (s + ar[i] <= e)
-//         {
-//             count += dice_random(s + ar[i], e, ar, dp);
-//         }
-//     }
-//     return dp[s] = count;
-// }
-// int dice_randomDP(int s, int e, vector<int> &ar, vector<int> &dp)
-// {
-//     for (s = e; s >= 0; s--)
-//     {
-//         if (s == e)
-//         {
-//             dp[s] = 1;
-//             continue;
-//         }
-//         if (dp[s] != 0)
-//             return dp[s];
-//         int count = 0;
-//         for (int i = 0; i < ar.size(); i++)
-//         {
-//             if (s + ar[i] <= e)
-//             {
-//                 count += dp[s + ar[i]];
-//             }
-//         }
-//         dp[s] = count;
-//     }
-//     return dp[0];
-// }
+int dice_random(int s, int e, vector<int> &ar, vector<int> &dp)
+{
+    if (s == e)
+    {
+        return dp[s] = 1;
+    }
+    if (dp[s] != 0)
+        return dp[s];
+    int count = 0;
+    for (int i = 0; i < ar.size(); i++)
+    {
+        if (s + ar[i] <= e)
+        {
+            count += dice_random(s + ar[i], e, ar, dp);
+        }
+    }
+    return dp[s] = count;
+}
+int dice_randomDP(int s, int e, vector<int> &ar, vector<int> &dp)
+{
+    for (s = e; s >= 0; s--)
+    {
+        if (s == e)
+        {
+            dp[s] = 1;
+            continue;
+        }
+        if (dp[s] != 0)
+            return dp[s];
+        int count = 0;
+        for (int i = 0; i < ar.size(); i++)
+        {
+            if (s + ar[i] <= e)
+            {
+                count += dp[s + ar[i]];
+            }
+        }
+        dp[s] = count;
+    }
+    return dp[0];
+}
 
-// //Leetcode 70. Climbing Stairs
-// int helper(int s, int n, vector<int> &dp)
-// {
-//     if (s == n)
-//         return dp[s] = 1;
-//     if (dp[s] != 0)
-//         return dp[s];
+//Leetcode 70. Climbing Stairs
+int helper(int s, int e, vector<int> &dp)
+{
+    if (s == e)
+        return dp[s] = 1;
+    if (dp[s] != 0)
+        return dp[s];
 
-//     int count = 0;
-//     if (s + 1 <= e)
-//         count += helper(s + 1, e, dp);
-//     if (s + 2 <= e)
-//         count += helper(s + 2, e, dp);
+    int count = 0;
+    if (s + 1 <= e)
+        count += helper(s + 1, e, dp);
+    if (s + 2 <= e)
+        count += helper(s + 2, e, dp);
 
-//     return dp[s] = count;
-// }
-// int climbStairs(int n)
-// {
-//     vector<int> dp(n + 1);
-//     return helper(0, n, dp);
-// }
+    return dp[s] = count;
+}
+int climbStairs(int n)
+{
+    vector<int> dp(n + 1);
+    return helper(0, n, dp);
+}
 
-// // Leetcode 746. Min Cost Climbing Stairs
-// int helper(int s, int n, vector<int> &cost, vector<int> &dp)
-// {
-//     if (s >= n - 2)
-//     {
-//         return dp[s] = cost[s];
-//     }
-//     if (dp[s] != 0)
-//         return dp[s];
-//     int count = min(helper(s + 1, n, cost, dp), helper(s + 2, n, cost, dp));
-//     return dp[s] = (count + cost[s]);
-// }
-// int minCostClimbingStairs(vector<int> &cost)
-// {
-//     int n = cost.size();
-//     vector<int> dp(n + 1);
-//     helper(0, n, cost, dp);
-//     return min(dp[0], dp[1]);
-// }
+// Leetcode 746. Min Cost Climbing Stairs
+int helper(int s, int n, vector<int> &cost, vector<int> &dp)
+{
+    if (s >= n - 2)
+    {
+        return dp[s] = cost[s];
+    }
+    if (dp[s] != 0)
+        return dp[s];
+    int count = min(helper(s + 1, n, cost, dp), helper(s + 2, n, cost, dp));
+    return dp[s] = (count + cost[s]);
+}
+int minCostClimbingStairs(vector<int> &cost)
+{
+    int n = cost.size();
+    vector<int> dp(n + 1);
+    helper(0, n, cost, dp);
+    return min(dp[0], dp[1]);
+}
 
 //Leetcode
 
@@ -765,7 +765,7 @@ int targetSum_02(int tar, int idx, vector<int> &arr, vector<vector<int>> &dp)
     return dp[idx][tar] = count;
 }
 
-int printPathOfTargetSum(int tar, int idx, vector<int> &arr, vector<vector<bool>> &dp, string ans)
+int printPathOfTargetSum(vector<int> &arr, int idx, int tar, string ans, vector<vector<bool>> &dp)
 { //Back engine
     if (tar == 0 || idx == 0)
     {
@@ -782,11 +782,11 @@ int printPathOfTargetSum(int tar, int idx, vector<int> &arr, vector<vector<bool>
     int count = 0;
     if (tar - arr[idx - 1] >= 0 && dp[idx - 1][tar - arr[idx - 1]])
     {
-        count += printPathOfTargetSum(tar - arr[idx - 1], idx - 1, arr, dp, ans + to_string(arr[idx - 1]));
+        count += printPathOfTargetSum(arr, idx - 1, tar - arr[idx - 1], ans + to_string(arr[idx - 1]), dp);
     }
     if (dp[idx - 1][tar])
     {
-        count += printPathOfTargetSum(tar, idx - 1, arr, dp, ans);
+        count += printPathOfTargetSum(arr, idx - 1, tar, ans, dp);
     }
     return dp[idx][tar] = count;
 }
@@ -798,7 +798,7 @@ void targetSum_02DP(vector<int> &coins, int tar)
     vector<vector<bool>> dp(coins.size() + 1, vector<bool>(tar + 1, false));
     for (int idx = 0; idx <= coins.size(); idx++)
     {
-        for (for (tar = 0; tar <= Tar; tar++))
+        for (tar = 0; tar <= Tar; tar++)
         {
             if (tar == 0 || idx == 0)
             {
@@ -814,9 +814,9 @@ void targetSum_02DP(vector<int> &coins, int tar)
                 continue;
             }
             int res = false;
-            if (tar - arr[idx - 1] >= 0)
+            if (tar - coins[idx - 1] >= 0)
             {
-                dp[idx][tar] = dp[idx - 1][tar - arr[idx - 1]];
+                dp[idx][tar] = dp[idx - 1][tar - coins[idx - 1]];
             }
 
             dp[idx][tar] = dp[idx][tar] || dp[idx - 1][tar];
@@ -859,9 +859,9 @@ int unbounded_Knapsack_2D_dp(vector<int> &w, vector<int> &v, int weight, int n, 
     int maxval = 0;
     if (weight - w[n - 1] >= 0)
     {
-        maxval = max(maxval, max(unbounded_Knapsack(w, v, weight - w[n - 1], n - 1, dp) + v[n - 1], unbounded_Knapsack(w, v, weight - w[n - 1], n, dp) + v[n - 1]));
+        maxval = max(maxval, max(unbounded_Knapsack_2D_dp(w, v, weight - w[n - 1], n - 1, dp) + v[n - 1], unbounded_Knapsack_2D_dp(w, v, weight - w[n - 1], n, dp) + v[n - 1]));
     }
-    maxval = max(maxval, unbounded_Knapsack(w, v, weight, n - 1, dp));
+    maxval = max(maxval, unbounded_Knapsack_2D_dp(w, v, weight, n - 1, dp));
     return dp[n][weight] = maxval;
 }
 
@@ -1214,6 +1214,7 @@ int maxEnvelopes(vector<vector<int>> &env)
 
 // leetcode 1235. Maximum Profit in Job Scheduling
 
+//---------------------------------------------------------------------------------------------------------------------------------
 void solve()
 {
     // int n = 10;
