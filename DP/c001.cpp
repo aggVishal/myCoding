9dc30eb8c77afa1172cad56f28ce5588f56da3aa
Vishalagg901 <aggarwalvishal901@gmail.com>
2020-07-07 23:38:55 +0530

    update

--- DP/c001.cpp
+++ DP/c001.cpp
@@ -1259,58 +1259,7 @@ int findNumberOfLIS(vector<int> &ar)
 
 //---------------------------------------------------------------------------------------------------------------------------------
 
-//cut Type problems
 
-int MCM_rec(vector<int> &arr, int si, int ei, vector<vector<int>> &dp)
-{
-    if (si + 1 == ei)
-    {
-        return dp[si][ei] = 0;
-    }
-
-    if (dp[si][ei] != -1)
-        return dp[si][ei];
-
-    int ans = 1e8;
-    for (int cut = si + 1; cut < ei; cut++)
-    {
-        int leftMatrix = MCM_rec(arr, si, cut, dp);
-        int rightMatrix = MCM_rec(arr, cut, ei, dp);
-        int cost = leftMatrix + (arr[si] * arr[cut] * arr[ei]) + rightMatrix;
-        ans = min(ans, cost);
-    }
-    return dp[si][ei] = ans;
-}
-int MCM_dp(vector<int> &arr)
-{
-    vector<vector<int>> dp(arr.size(), vector<int>(arr.size(), -1));
-    for (int gap = 1; gap < arr.size(); gap++)
-    {
-        for (int si = 0, ei = gap; ei < arr.size(); si++, ei++)
-        {
-            if (si + 1 == ei)
-            {
-                return dp[si][ei] = 0;
-            }
-
-            if (dp[si][ei] != -1)
-                return dp[si][ei];
-
-            int ans = 1e8;
-            for (int cut = si + 1; cut < ei; cut++)
-            {
-                int leftMatrix = MCM_rec(arr, si, cut, dp);
-                int rightMatrix = MCM_rec(arr, cut, ei, dp);
-                int cost = leftMatrix + (arr[si] * arr[cut] * arr[ei]) + rightMatrix;
-                ans = min(ans, cost);
-            }
-            return dp[si][ei] = ans;
-        }
-    }
-}
-
-// Optimal Binary Search Tree
-// https://www.geeksforgeeks.org/optimal-binary-search-tree-dp-24/
 
 void solve()
 {
