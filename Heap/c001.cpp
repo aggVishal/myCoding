886d4ab8caaccb8489adc847ac869df9c8ac9f7a
Vishalagg901 <aggarwalvishal901@gmail.com>
2020-07-16 18:35:29 +0530

    update

--- Heap/c001.cpp
+++ Heap/c001.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_map>
 using namespace std;
 
 class heap
@@ -10,7 +13,7 @@ public:
     {
         for (int ele : list)
         {
-            arr.push_back(lis);
+            arr.push_back(ele);
         }
 
         for (int i = arr.size() - 1; i <= 0; i--)
@@ -48,11 +51,18 @@ public:
             minIdx = pi;
         }
 
-        if (maxIdx != idx)
+        if (minIdx != idx)
         {
             swap(minIdx, idx);
             upHeapify(minIdx);
         }
+    }
+
+    void swap(int &a, int &b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
     }
 
     bool isEmpty()
@@ -76,7 +86,7 @@ public:
         if (arr.size() == 0)
             return;
 
-        swap(ar[0], arr[arr.size() - 1]);
+        swap(arr[0], arr[arr.size() - 1]);
         arr.pop_back();
         downHeapify(0, arr.size());
     }
@@ -85,4 +95,112 @@ public:
         arr.push_back(ele);
         upHeapify(arr.size() - 1);
     }
+};
+
+// Leetcode 215. Kth Largest Element in an Array
+int findKthLargest(vector<int> &nums, int k)
+{
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for (int ele : nums)
+    {
+        pq.push(ele);
+        if (pq.size() > k)
+        {
+            pq.pop();
+        }
+    }
+    return pq.top();
+}
+
+// Leetcode 703. Kth Largest Element in a Stream
+class KthLargest
+{
+public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int x = -1;
+    KthLargest(int k, vector<int> &nums)
+    {
+        x = k;
+        for (int ele : nums)
+        {
+            pq.push(ele);
+            if (pq.size() > x)
+            {
+                pq.pop();
+            }
+        }
+    }
+
+    int add(int val)
+    {
+        pq.push(val);
+        if (pq.size() > x)
+        {
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
+
+// Leetcode 378. Kth Smallest Element in a Sorted Matrix
+int kthSmallest01(vector<vector<int>> &mat, int k)
+{
+    priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+    //priority_queue<vector<int>>pq;
+    for (int i = 0; i < mat.size(); i++)
+    {
+        pq.push({mat[i][0], i, 0});
+    }
+    while (k-- > 1)
+    {
+        vector<int> idx = pq.top();
+        pq.pop();
+        if (++idx[2] < mat[0].size())
+        {
+            pq.push({mat[idx[1]][idx[2]], idx[1], idx[2]});
+        }
+    }
+    return pq.top()[0];
+}
+int kthSmallest02(vector<vector<int>> &mat, int k)
+{
+    //priority_queue<vector<int> , vector<vector<int>> , greater<vector<int>> >pq;
+    priority_queue<vector<int>> pq;
+    for (int i = 0; i < mat.size(); i++)
+    {
+        pq.push({-mat[i][0], i, 0});
+    }
+    while (k-- > 1)
+    {
+        vector<int> idx = pq.top();
+        pq.pop();
+        if (++idx[2] < mat[0].size())
+        {
+            pq.push({-mat[idx[1]][idx[2]], idx[1], idx[2]});
+        }
+    }
+    return -pq.top()[0];
+}
+
+// Leetcode 347. Top K Frequent Elements
+vector<int> topKFrequent(vector<int> &nums, int k)
+{
+    unordered_map<int, int> freq;
+    for (int ele : nums)
+    {
+        freq[ele]++;
+    }
+    unordered_map<int, int>::iterator it;
+    priority_queue<vector<int>> pq;
+    for (it = freq.begin(); it != freq.end(); it++)
+    {
+        pq.push({it->second, it->first});
+    }
+    vector<int> ans;
+    while (k--)
+    {
+        ans.push_back(pq.top()[1]);
+        pq.pop();
+    }
+    return ans;
 }
